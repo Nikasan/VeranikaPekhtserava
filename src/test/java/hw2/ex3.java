3fc521a3447fac6321b0fbfdcbd1f4559d759736
Nikasan <vvlobkova@gmail.com>
2019-01-29 18:37:33 +0300

    add  HW 2 ex3 testngEx3.xml

--- src/test/java/hw2/ex3.java
+++ src/test/java/hw2/ex3.java
@@ -1,12 +1,10 @@
-package hw1;
+package hw2;
 
 import base.SeleniumBase;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.*;
 
 import java.util.Arrays;
